00c3db61609e8ccfa56340aec06cf3cc0a50009e
srkrishna3384 <srkrishna3384@gmail.com>
2020-03-16 23:05:39 +0530

    webdrivermanager

--- src/test/java/maven/demo/SampleMavenProj/TestDemo.java
+++ src/test/java/maven/demo/SampleMavenProj/TestDemo.java
@@ -1,14 +1,18 @@
 package maven.demo.SampleMavenProj;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class TestDemo {
+	
 	@Test
 public static void m1() {
 	System.out.println("TestDemo class");
-	System.setProperty("webdriver.chrome.driver", "D:\\SampleMavenProj\\Drivers\\chromedriver.exe");
+	//System.setProperty("webdriver.chrome.driver", "D:\\SampleMavenProj\\Drivers\\chromedriver.exe");
+	WebDriverManager.chromedriver().setup();
 	WebDriver driver = new ChromeDriver();
 	System.out.println("Browser launched successfully.........");
 	driver.get("https://www.gmail.com");
