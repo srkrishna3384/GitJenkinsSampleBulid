f07cd3270709f6c18f76c0118a24469b2c6a1b32
srkrishna3384 <srkrishna3384@gmail.com>
2020-03-17 07:50:39 +0530

    Added new file

--- src/test/java/maven/demo/SampleMavenProj/TestDemo.java
+++ src/test/java/maven/demo/SampleMavenProj/TestDemo.java
@@ -11,11 +11,6 @@ public class TestDemo {
 	@Test
 public static void m1() {
 	System.out.println("TestDemo class");
-	//System.setProperty("webdriver.chrome.driver", "D:\\SampleMavenProj\\Drivers\\chromedriver.exe");
-	WebDriverManager.chromedriver().setup();
-	WebDriver driver = new ChromeDriver();
-	System.out.println("Browser launched successfully.........");
-	driver.get("https://www.gmail.com");
-	System.out.println("Navigated Gmail Page...........");
-}
+	
+	}
 }
